9e5570350745ba57fe69f2c391900143bd715ae4
Elvis <elvis.omondi@student.moringaschool.com>
2024-08-17 10:14:27 +0300

    commit

---
+++ index.js
@@ -0,0 +1,11 @@
+/* **Example 3:**
+   ```javascript
+   const numbers = [1, 2, 3, 4, 5];
+   const doubleNumbers = [];
+   for (let i = 0; i < numbers.length; i++) {
+       doubleNumbers.push(numbers[i] * 2);
+   }
+   console.log(doubleNumbers);
+   ```*/
+
+   
